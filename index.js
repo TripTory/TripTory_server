85ace3707c9328fa07cd76106805b27ac2de9063
hyni03 <hyni03@naver.com>
2024-04-02 15:02:16 +0900

    Feat: connect to MongoDB and set up server endpoint #6

--- index.js
+++ index.js
@@ -2,6 +2,8 @@
 const express = require("express"); 
 const cors = require("cors");
 const dotenv = require("dotenv"); // .env 파일 사용 라이브러리
+const mongoose = require('mongoose');
+const axios = require('axios');
 
 const bodyParser = require("body-parser"); // body-parser 라이브러리 사용
 dotenv.config(); // dotevn 라이브러리 사용
@@ -12,6 +14,23 @@ const app = express();
 const port = process.env.PORT;
 
 app.use(express.json());
+
+// connect mongoose
+const mongoose = require('mongoose');
+
+mongoose.connect(
+    process.env.MONGODB_URI,
+    {
+        // useNewUrlPaser: true,
+        // useUnifiedTofology: true,
+        // useCreateIndex: true,
+        // useFindAndModify: false,
+    }
+)
+.then(() => console.log('MongoDB connected'))
+.catch((err) => {
+    console.log(err);
+});
 
 app.get('/', function (req, res) {
     res.send("Server Start!!");
