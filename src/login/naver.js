4327720fc5ea8b49b7ba2847726e635880f225f4
hyni03 <hyni03@naver.com>
2024-04-30 14:08:01 +0900

    Feat: add cookie configuration for session management #16

--- src/login/naver.js
+++ src/login/naver.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const router = express.Router();
 const axios = require('axios');
+const cookieParser = require('cookie-parser'); // 쿠키 파서 미들웨어 추가
+const session = require('express-session'); // 세션 미들웨어 추가
 
 const { User } = require('../user/user_schema');
 
@@ -49,7 +51,7 @@ router.get('/callback', async (req, res) => {
       // 데이터베이스에서 사용자 정보 조회
       let user = await User.findOne({ oauthId: naveruserData.id });
 
-      // 새로운 사용자인지 확인
+      // 새로운 사용자인지 확인하고 세션 및 쿠키 생성
       if (!user) {
         user = new User({
           oauthId: naveruserData.id,
@@ -60,9 +62,15 @@ router.get('/callback', async (req, res) => {
 
         await user.save(); // 새로운 사용자 정보 저장
 
+        req.session.userId = user._id; 
+        res.cookie('userSession', JSON.stringify(req.session), { maxAge: 86400 * 1000 }); // 세션에 쿠키 저장, 유효기간 1일
+
         res.json({ message: '회원가입 성공', email: naveruserData.email });
       } else {
         // 기존 사용자인 경우 로그인 메시지 응답
+        req.session.userId = user._id; 
+        res.cookie('userSession', JSON.stringify(req.session), { maxAge: 86400 * 1000 }); // 세션에 쿠키 저장, 유효기간 1일
+
         res.json({ message: '로그인 성공', email: naveruserData.email });
       }
 
