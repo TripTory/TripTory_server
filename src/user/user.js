05275302f2ee0e8bbc05d90a2a3ce4c524958332
hyni03 <hyni03@naver.com>
2024-04-30 13:16:16 +0900

    Feat: add endpoint to modify user info #17

--- src/user/user.js
+++ src/user/user.js
@@ -29,6 +29,41 @@ router.get('/', async (req, res) => {
 });
 
 
+router.put('/', async (req, res) => {
+    console.log('사용자 정보 수정 요청');
+    try {
+        const user = await User.findById(req.body.userId);
+        if(user){
+            console.log('수정할 사용자 ID:', user._id);
+
+            const updateuser = await User.findByIdAndUpdate(req.body.userId, {
+                name: req.body.name,
+                email: req.body.email
+            }, {new: true} );
+
+            if (updateuser) {
+                console.log('사용자 정보 수정 완료');
+                return res.status(200).json({
+                success: true,
+                travel: updateuser
+                });
+            } else {
+                console.log('사용자 정보 수정 실패');
+                return res.status(400).json({ success : false, message : '사용자 정보 수정 실패'});
+            }
+
+        } else {
+        console.log('사용자를 찾을 수 없습니다.');
+        return res.status(404).json({ success : false, message : '사용자를 찾을 수 없습니다.' });
+        }
+    } catch (err) {
+        console.error(err);
+        return res.status(500).json({ success: false, message: '서버 오류' });
+    }
+});
+
+
+
   
 
 
