4fbd81ec04129b7d7db4af221f7e9c66576d80ae
hyni03 <hyni03@naver.com>
2024-05-04 00:43:23 +0900

    Update: fix error status code in diary.js #24

--- src/diary/diary.js
+++ src/diary/diary.js
@@ -13,6 +13,7 @@ router.post('/', async (req, res) => {
       const travel = await Travel.findById(req.body.travel);
       if(travel){
           console.log('여행 ID:', travel._id);
+
           if (user) {
               console.log('사용자 ID:', user._id);
   
@@ -27,10 +28,9 @@ router.post('/', async (req, res) => {
         
               const savedDiary = await diary.save(); // 여행 객체 저장
               
-              return res.status(200).json({
-                success: true,
-                diaryInfo: savedDiary,
-              });
+              console.log('일기 생성 완료');
+              return res.status(200).json({ success: true, message: '일기 생성 완료', diaryInfo: savedDiary});
+
           } else {
             console.log('사용자를 찾을 수 없습니다.');
             return res.status(404).json({ success: false, message: '사용자를 찾을 수 없습니다.' });
@@ -41,7 +41,7 @@ router.post('/', async (req, res) => {
       }
     } else {
       console.log('로그인이 필요합니다.');
-      return res.status(400).json({ success: false, message: '로그인이 필요합니다.' });
+      return res.status(401).json({ success: false, message: '로그인이 필요합니다.' });
     }
   } catch (err) {
     console.error(err);
@@ -60,23 +60,21 @@ router.delete('/:diaryId', async(req, res) => {
   
         if (diary.userId != req.session.userId) {
           console.log('일기에 대한 권한이 없습니다.');
-          return res.status(400).json({ success: false, message: '일기에 대한 권한이 없습니다.' });
+          return res.status(403).json({ success: false, message: '일기에 대한 권한이 없습니다.' });
         }
   
         await Diary.findByIdAndDelete(diary._id);
   
-        return res.status(200).json({
-          success: true,
-          message: '일기 삭제 완료'
-        });
+        console.log('일기 삭제 완료');
+        return res.status(200).json({ success: true, message: '일기 삭제 완료' });
   
       } else {
         console.log('일기를 찾을 수 없습니다.');
-        return res.status(400).json({ success: false, message: '일기를 찾을 수 없습니다.'});
+        return res.status(404).json({ success: false, message: '일기를 찾을 수 없습니다.'});
       }
     } else {
       console.log('로그인이 필요합니다.');
-      return res.status(400).json({ success: false, message: '로그인이 필요합니다.'});
+      return res.status(401).json({ success: false, message: '로그인이 필요합니다.'});
     }
   } catch(err){
     console.error(err);
@@ -95,30 +93,26 @@ router.put('/:diaryId', async(req,res) => {
   
         if(diary.userId != req.session.userId){
           console.log('일기에 대한 권한이 없습니다.');
-          return res.status(400).json({ success: false, message: '일기에 대한 권한이 없습니다.' });
+          return res.status(403).json({ success: false, message: '일기에 대한 권한이 없습니다.' });
         }
         
-        const updatediary = await Diary.findByIdAndUpdate(req.params.diaryId, {
+        await Diary.findByIdAndUpdate(req.params.diaryId, {
           title: req.body.title,
           content: req.body.content,
           location: req.body.location,
           date: req.body.date
         }, { new: true });
   
-        if(updatediary){
-          console.log('일기 수정 완료');
-          return res.status(200).json({ success: true, message: '일기 수정 완료' });
-        } else {
-          console.log('일기 수정 실패');
-          return res.status(400).json({ success: false, message: '일기 수정 실패'});
-        }
+        console.log('일기 수정 완료');
+        return res.status(200).json({ success: true, message: '일기 수정 완료' });
+
       } else {
         console.log('해당 일기를 찾을 수 없습니다.');
         return res.status(404).json({ success: false, message: '해당 일기를 찾을 수 없습니다.' });
       }
     } else {
       console.log('로그인이 필요합니다.');
-      return res.status(400).json({ success: false, message: '로그인이 필요합니다.' });
+      return res.status(401).json({ success: false, message: '로그인이 필요합니다.' });
     }
   } catch(err) {
     console.log(err);
@@ -143,7 +137,7 @@ router.get('/', async(req, res) => {
       }
     } else {
       console.log('로그인이 필요합니다.');
-      return res.status(400).json({ success: false, message: '로그인이 필요합니다.' });
+      return res.status(401).json({ success: false, message: '로그인이 필요합니다.' });
     }
   } catch (err) {
     console.log(err);
