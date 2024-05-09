76bc0b991260e3277d8cfc53b11e0ab92986a88c
hyni03 <hyni03@naver.com>
2024-05-09 13:46:12 +0900

    Feat: add diaryimg save to storage and db #38

--- src/diary/diary.js
+++ src/diary/diary.js
@@ -1,9 +1,26 @@
 const express = require('express');
 const router = express.Router();
+const cookieParser = require('cookie-parser'); // 쿠키 파서 미들웨어 추가
+const session = require('express-session'); 
+const { Storage } = require('@google-cloud/storage');
+const multer = require('multer');
 
 const { User } = require('../user/user_schema');
 const { Travel } = require('../travel/travel_schema');
 const { Diary} = require('./diary_schema');
+
+// Google Cloud Storage 설정
+const storage = new Storage({
+  projectId: process.env.GCP_PROJECT_ID, // GCP 프로젝트 ID
+  keyFilename: process.env.STORAGE_KEYFILE // 서비스 계정 키 파일 경로
+});
+
+const bucketName = process.env.STORAGE_BUCKET_NAME; // GCS 버킷 이름
+const bucket = storage.bucket(bucketName);
+
+// Multer 설정
+const multerStorage = multer.memoryStorage();
+const upload = multer({ storage: multerStorage });
 
 router.get('/', async(req, res) => {
   console.log('일기 목록 요청');
@@ -70,36 +87,43 @@ router.get('/:diaryId', async(req, res) => {
   }
 });
 
-router.post('/', async (req, res) => {
+router.post('/', upload.array('images', 10), async (req, res) => {
   console.log("일기 생성 요청");
   try {
     if (req.session && req.session.userId){
       const user = await User.findById(req.session.userId);
       const travel = await Travel.findById(req.body.travel);
       if(travel){
-          console.log('여행 ID:', travel._id);
+        console.log('여행 ID:', travel._id);
 
-          if (user) {
-              console.log('사용자 ID:', user._id);
-  
-              const diary = new Diary({
-                title: req.body.title,
-                content: req.body.content,
-                location: req.body.location,
-                date: req.body.date,
-                travel: [travel._id],
-                userId: [user._id]
-              });
-        
-              const savedDiary = await diary.save(); // 여행 객체 저장
-              
-              console.log('일기 생성 완료');
-              return res.status(200).json({ success: true, message: '일기 생성 완료', diaryInfo: savedDiary});
+        if (user) {
+          console.log('사용자 ID:', user._id);
 
-          } else {
-            console.log('사용자를 찾을 수 없습니다.');
-            return res.status(404).json({ success: false, message: '사용자를 찾을 수 없습니다.' });
-          } 
+          const diary = new Diary({
+            title: req.body.title,
+            content: req.body.content,
+            location: req.body.location,
+            date: req.body.date,
+            travel: [travel._id],
+            userId: [user._id]
+          });
+
+          const files = req.files;
+          for (const file of files) {
+            tags = []; // tag 분류 알고리즘 추가
+            const img = storage.bucket(bucketName).file(`diary/${diary._id}/${file.originalname}`);
+            await img.save(file.buffer);
+            diary.img.push({ imgpath: img.publicUrl(), tag: tags });
+          }
+
+          await diary.save(); // 여행 객체 저장
+          
+          console.log('일기 생성 완료');
+          return res.status(200).json({ success: true, message: '일기 생성 완료', diaryInfo: diary});
+        } else {
+          console.log('사용자를 찾을 수 없습니다.');
+          return res.status(404).json({ success: false, message: '사용자를 찾을 수 없습니다.' });
+        } 
       } else {
         console.log('여행을 찾을 수 없습니다.');
         return res.status(404).json({ success: false, message: '여행을 찾을 수 없습니다.' });
@@ -166,7 +190,12 @@ router.delete('/:diaryId', async(req, res) => {
           return res.status(403).json({ success: false, message: '일기에 대한 권한이 없습니다.' });
         }
   
-        await Diary.findByIdAndDelete(diary._id);
+        await Diary.findByIdAndDelete(diary._id);    
+
+        const [files] = await storage.bucket(bucketName).getFiles({
+          prefix: `diary/${diary._id}`,
+        });
+        await Promise.all(files.map(file => file.delete()));
   
         console.log('일기 삭제 완료');
         return res.status(200).json({ success: true, message: '일기 삭제 완료' });
