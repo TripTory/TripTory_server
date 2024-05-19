7a31c8d0bd31439846b0471d43c02156901d2b29
hyni03 <hyni03@naver.com>
2024-05-19 19:22:15 +0900

    Fix: fix merge error #54

--- src/login/kakao.js
+++ src/login/kakao.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const axios = require('axios');
-const cors = require('cors');
+const cors = require("cors");
 const { User } = require('../user/user_schema');
 const dotenv = require("dotenv"); 
 require('dotenv').config();
@@ -9,20 +9,15 @@ require('dotenv').config();
 dotenv.config();
 
 const corsOptions = {
-  origin: process.env.FRONT_URL, // 허용할 출처
-  credentials: true // 인증 정보 허용
+  origin: process.env.FRONT_URL,
+  credentials: true
 };
 
 router.use(cors(corsOptions));
 
 router.get('/', (req, res) => {
-  try {
-    const authorizationUrl = `https://kauth.kakao.com/oauth/authorize?client_id=${process.env.KAKAO_ID}&redirect_uri=${process.env.KAKAO_URI}&response_type=code`;
-    res.json(authorizationUrl);
-  } catch (error) {
-    console.error("Failed to generate Naver OAuth authorization URL:", error);
-    res.status(500).json({ error: "Failed to generate authorization URL" });
-  }
+  const oauthUrl = `https://kauth.kakao.com/oauth/authorize?client_id=${process.env.KAKAO_ID}&redirect_uri=${process.env.KAKAO_URI}&response_type=code`;
+  res.redirect(oauthUrl);
 });
 
 router.get('/callback', async (req, res) => {
@@ -68,8 +63,7 @@ router.get('/callback', async (req, res) => {
         req.session.userId = user._id;
         res.cookie('userSession', JSON.stringify(req.session), { maxAge: 86400 * 1000 });
 
-        //res.json({ message: '회원가입 성공', email: kakaoUserData.kakao_account.email });
-        res.redirect(`${process.env.FRONT_URL}/join`);
+        res.json({ message: '회원가입 성공', email: kakaoUserData.kakao_account.email });
       }
       else {
         req.session.userId = user._id;
@@ -79,23 +73,20 @@ router.get('/callback', async (req, res) => {
           oauthAccessToken: accessToken
         }, { new: true });
 
-        // res.json({ message: '로그인 성공', email: kakaoUserData.kakao_account.email });
-        res.redirect(`${process.env.FRONT_URL}/home`);
+        res.json({ message: '로그인 성공', email: kakaoUserData.kakao_account.email });
       }
     } catch (error) {
       if (error.code === 11000 && error.keyPattern.email) {
         // 중복된 이메일 주소로 인한 오류
-        console.error('이미 가입된 이메일입니다. 다른 방식으로 로그인해주세요.' });
-        res.status(400).json({ error: '이미 가입된 이메일입니다. 다른 방식으로 로그인해주세요.' });
-        res.status(500).redirect(`${process.env.FRONT_URL}/login`); // 오류 발생 시 로그인 화면으로 리다이렉트
+        return res.status(400).json({ error: '이미 가입된 이메일입니다. 다른 방식으로 로그인해주세요.' });
       } else {
         console.error('사용자 정보 요청 실패:', error);
-        res.status(500).redirect(`${process.env.FRONT_URL}/login`); // 오류 발생 시 로그인 화면으로 리다이렉트
+        res.status(500).send('사용자 정보 요청 실패');
       }
     }
   } catch (error) {
     console.error('토큰 요청 실패:', error);
-    res.status(500).redirect(`${process.env.FRONT_URL}/login`); // 오류 발생 시 로그인 화면으로 리다이렉트
+    res.status(500).send('토큰 요청 실패');
   }
 });
 
