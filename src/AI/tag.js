bd15e2d6085b28201bef5f2bf7f4ece498500d7d
hyni03 <hyni03@naver.com>
2024-05-28 13:02:16 +0900

    Fix: fix API error for retrieving images related to a tag #93

--- src/AI/tag.js
+++ src/AI/tag.js
@@ -38,7 +38,7 @@ async function getSignedUrl(diaryId, img) {
   };
 
   try {
-    const [url] = await bucket.file(`diary/${diaryId}/${img.imgpath}`).getSignedUrl(options);
+    const [url] = await bucket.file(`diary/${diaryId}/${img._id}`).getSignedUrl(options);
     return url;
   } catch (err) {
     console.error('이미지 URL 생성 실패:', err);
@@ -47,7 +47,7 @@ async function getSignedUrl(diaryId, img) {
 }
 
 router.get('/', async(req, res) => {
-  console.log('태그 이미지 요청: ');
+  console.log("각 태그별 대표 이미지 요청")
   try {
     if (req.session && req.session.userId) {
       const diarys = await Diary.find({ userId: req.session.userId });
@@ -56,47 +56,61 @@ router.get('/', async(req, res) => {
         console.log("사용자 일기에 사진이 없습니다.")
         return res.status(404).json({ success: false, message: "사용자 일기에 사진이 없습니다." })
       }
-      const tagName = req.body.tag;
 
-      if(!req.body.tag){
-        console.log("각 태그별 대표 이미지 요청")
-        const imageTags = {};
-        for (const diary of diarys) {
-          if (diary.img) 
-            for (const img of diary.img) 
-              if (img.tag) 
-                for (const tag of img.tag) 
-                  if (!imageTags[tag]) {
-                    const url = await getSignedUrl(diary._id, img);
-                    imageTags[tag] = url; // 새로운 태그인 경우 해당 이미지로 초기화
-                  }
-        }
-  
-        return res.status(200).json({
-          success: true,
-          imageTags
-        });
-      } 
-      
-      else {
-        //const tagName = req.body.tag;
-        console.log(`${tagName}에 대한 이미지 요청`)
-
-        let images = [];
-        for (const diary of diarys) {
-          if (diary.img) 
-            for (const img of diary.img) 
-              if (img.tag && img.tag.includes(tagName)) {
-                const url = await getSignedUrl(diary._id, img);
-                images.push(url);
-              }
-        }
-  
-        return res.status(200).json({
-          success: true,
-          images
-        });
+      const imageTags = {};
+      for (const diary of diarys) {
+        if (diary.img) 
+          for (const img of diary.img) 
+            if (img.tag) 
+              for (const tag of img.tag) 
+                if (!imageTags[tag]) {
+                  const url = await getSignedUrl(diary._id, img);
+                  imageTags[tag] = url; // 새로운 태그인 경우 해당 이미지로 초기화
+                }
       }
+      
+      return res.status(200).json({
+        success: true,
+        imageTags
+      });
+
+    } else {
+      console.log('로그인이 필요합니다.');
+      return res.status(401).json ({ success: false, message: '로그인이 필요합니다.' });
+    }
+  } catch (err) {
+    console.error(err);
+    return res.status(500).json({ success: false, message: '서버 오류' });
+  }
+})
+
+router.get('/:tagName', async(req, res) => {
+  console.log(`${tagName}에 대한 이미지 요청`)
+  try {
+    if (req.session && req.session.userId) {
+      const diarys = await Diary.find({ userId: req.session.userId });
+
+      if(diarys.length <= 0){
+        console.log("사용자 일기에 사진이 없습니다.")
+        return res.status(404).json({ success: false, message: "사용자 일기에 사진이 없습니다." })
+      }
+      const tagName = req.params.tagName;
+
+      let images = [];
+      for (const diary of diarys) {
+        if (diary.img) 
+          for (const img of diary.img) 
+            if (img.tag && img.tag.includes(tagName)) {
+              const url = await getSignedUrl(diary._id, img);
+              images.push(url);
+            }
+      }
+  
+      return res.status(200).json({
+        success: true,
+        images
+      });
+
     } else {
       console.log('로그인이 필요합니다.');
       return res.status(401).json ({ success: false, message: '로그인이 필요합니다.' });
